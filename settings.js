8a0f0b9ffddd744a5a10680c02358c426c35d66e
NAMods <97587103+NAMods@users.noreply.github.com>
2023-04-18 12:09:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,7 +27,7 @@ global.socialm = "GitHub: Dark" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.session = " " //session Id
+global.session = "MDdhRHhVbjI=" //session Id
 global.ownernomer = "94770802376" //ur number
 global.lang = "SI" //Select Language "EN" to English "SI" to sinhala 
 global.premium = ['94770802376'] //ur premium number
